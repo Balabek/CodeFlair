a53a1e4389938fda216e3a9a086efc508a0a2661
Balabek <vonbabayan@gmail.com>
2019-03-09 18:21:45 +0300

    "Location" inner page is coded (responsiveness fully applied).

--- js/codeflair.js
+++ js/codeflair.js
@@ -40,6 +40,47 @@ $(document).ready(function(){
     });
 
 
+
+    // Location Slider
+    $('.codeflair_location_slider').slick({
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        arrows: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+
+
     // Mini-Map Slider ("Job" inner page)
     $('.mini_map_slider').slick({
         dots: true,
@@ -90,11 +131,6 @@ $(document).ready(function(){
     $('.counter').counterUp({
         time: 1100
     });
-
-
-
-
-
 
 
 });
