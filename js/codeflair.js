01af273bd828857e50ad0c212f78580c4f2bf6e4
Balabek <vonbabayan@gmail.com>
2020-12-06 14:55:17 +0300

    Final version

--- js/codeflair.js
+++ js/codeflair.js
@@ -1,96 +1,5 @@
 $(document).ready(function(){
 
-
-    // Homepage Slider
-    $('.codeflair_home_slider').slick({
-        dots: false,
-        autoplay: true,
-        infinite: true,
-        arrows: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 580,
-                settings: {
-                    dots: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-        ]
-    });
-
-
-
-    // Location Slider
-    $('.codeflair_location_slider').slick({
-        dots: false,
-        autoplay: true,
-        infinite: true,
-        arrows: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 580,
-                settings: {
-                    dots: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-        ]
-    });
-
-
-
-    // Mini-Map Slider ("Job" inner page)
-    $('.mini_map_slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: false
-    });
-
-
     // Animating the standard hamburger-menu
     $(".navbar-toggle").on("click", function () {
         $(this).toggleClass("active");
@@ -130,6 +39,112 @@ $(document).ready(function(){
     // Counter-Up (has dependency with 'waypoints.js')
     $('.counter').counterUp({
         time: 1100
+    });
+
+
+    // Mini-Map Slider ("Job" inner page)
+    $('.mini_map_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: false
+    });
+
+
+
+    // Homepage Slider
+    let homepageSlider = $('.codeflair_home_slider');
+    $(homepageSlider).slick({
+        dots: false,
+        autoplay: false,
+        infinite: true,
+        arrows: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+
+    $(homepageSlider).slickLightbox({
+        itemSelector: 'a',
+        navigateByKeyboard: true,
+        background: 'rgba(0,0,0,.93)'
+    });
+
+
+
+    // Location Slider
+    let locationInnerPageSlider = $('.codeflair_location_slider');
+    $(locationInnerPageSlider).slick({
+        dots: false,
+        autoplay: false,
+        infinite: true,
+        arrows: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    dots: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+
+
+    $(locationInnerPageSlider).slickLightbox({
+        itemSelector: 'a',
+        navigateByKeyboard: true,
+        background: 'rgba(0,0,0,.93)'
     });
 
 
